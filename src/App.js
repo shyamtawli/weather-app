5f488d1a1f72a2598c112483d3899592f06a4233
shyamtawli <shyamtawli2@gmail.com>
2023-04-08 19:08:11 +0530

    search dropdown

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Search from "./components/Search/Search";
+
+function App() {
+  const handleonSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleonSearchChange} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/API.jsx
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c2301b1f3bmsha4b48944d896a95p1b0f87jsn662cfd94e080",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
